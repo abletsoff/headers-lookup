9763fafe14fa6d3fc297598510c8f06eb3cb336d
Andrew Abletsov <abletsoff@gmail.com>
2023-02-14 13:37:23 +0300

    Update http_lookup.sh

--- http_lookup.sh
+++ http_lookup.sh
@@ -2,40 +2,48 @@
 
 user_agent="Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"
 
-disclosure_headers=('Server' 'Liferay-Portal' 'X-Turbo-Charged-By' 'X-Powered-By' \
-		'X-Server-Powered-By' 'X-Powered-CMS' 'SourceMap or X-SourceMap' 'X-AspNetMvc-Version' \
-		'X-AspNet-Version' 'X-SourceFiles' 'X-Redirect-By' 'X-Generator' 'X-Generated-By' 'X-CMS')
+# last update: "2023-01-12 21:19:59"
+disclosure_headers=("Host-Header" \
+    "Liferay-Portal" \
+    "Pega-Host" \
+    "Powered-By" \
+    "Product" \
+    "Server" \
+    "SourceMap" \
+    "X-AspNet-Version" \
+    "X-AspNetMvc-Version" \
+    "X-Atmosphere-error" \
+    "X-Atmosphere-first-request" \
+    "X-Atmosphere-tracking-id" \
+    "X-CF-Powered-By" \
+    "X-CMS" \
+    "X-Content-Encoded-By" \
+    "X-Envoy-Upstream-Service-Time" \
+    "X-Framework" \
+    "X-Generated-By" \
+    "X-Generator" \
+    "X-Mod-Pagespeed" \
+    "X-Page-Speed" \
+    "X-Php-Version" \
+    "X-Powered-By" \
+    "X-Powered-By-Plesk" \
+    "X-Powered-CMS" \
+    "X-Redirect-By" \
+    "X-Server-Powered-By" \
+    "X-SourceFiles" \
+    "X-SourceMap" \
+    "X-Turbo-Charged-By" \
+    "X-Varnish-Backend" \
+    "X-Varnish-Server")
 
-security_headers=('Strict-Transport-Security' 'Content-Security-Policy' 'X-Frame-Options' \
-		'X-Content-Type-Options' 'Permissions-Policy' 'Referrer-Policy')
+security_headers=("Strict-Transport-Security" \
+   	"Content-Security-Policy" \
+   	"X-Frame-Options" \
+	"X-Content-Type-Options" \
+	"Permissions-Policy" \
+   	"Referrer-Policy")
+
 ip_address_regex='([1-2]?\d{1,2}\.){3}[1-2]?\d{1,2}'
-
-Red='\033[0;91m'
-Yellow='\033[0;93m'
-Green='\033[0;92m'
-Color_Off='\033[0m' 
-
-function f_dns_resolve () {
-
-	domain=$1
-   	ip_address=$(grep -P $ip_address_regex <<< $domain)
-	
-	if [[ $ip_address == "" ]]; then
-        
-        IFS=':'; read -ra url_array <<< $domain; IFS=' ' # split port number
-        domain_part=${url_array[0]}
-
-   		resolve=$(dig +short $domain_part | head -n 1)	
-    	if [[ $resolve == "" ]]; then
-    	    resolve="Unresolved"
-		    echo -e "$domain - ${Red}$resolve${Color_Off}"
-        else
-		    echo -e "$domain - $resolve"
-		fi
-	else
-		echo -e "$domain"
-	fi
-}
 
 function f_http_parse () {
 	
@@ -46,31 +54,31 @@ function f_http_parse () {
 	http_response=$(curl -A "${user_agent}" --connect-timeout 5 -v $1 2>&1)
 	
 	if [[ $(grep "^curl: (28)" <<< $http_response) != "" ]]; then
-		echo -e "\t${Red}Connection timeout${Color_Off}"
+		echo -e "Connection timeout"
 		return 0
 	elif [[ $(grep "^curl: (7)" <<< $http_response) != "" ]]; then
-		echo -e "\t${Red}Failed to connect${Color_Off}"
+		echo -e "Failed to connect"
 		return 0
 	elif [[ $(grep "^curl: (56)" <<< $http_response) != "" ]]; then
-		echo -e "\t${Red}Connection reset${Color_Off}"
+		echo -e "Connection reset"
 		return 0
 	elif [[ $(grep "^curl: (60)" <<< $http_response) != "" ]]; then
-        echo -en "\t${Red}(SSL certificate problem)${Color_Off}"
+        echo "Warning: (SSL certificate problem)"
 	    http_response=$(curl -A "${user_agent}" --connect-timeout 5 -v -k $1 2>&1)
 	elif [[ $(grep "^curl: (3)" <<< $http_response) != "" ]]; then
-		echo -e "\t${Red}URL bad/illegal format${Color_Off}"
+		echo -e "URL bad/illegal format"
 		return 0
 	elif [[ $(grep "^curl: (52)" <<< $http_response) != "" ]]; then
-		echo -e "\t${Red}Empty reply from server${Color_Off}"
+		echo -e "Empty reply from server"
 		return 0
 	elif [[ $(grep "^curl: (35)" <<< $http_response) != "" ]]; then
-		echo -e "\t${Red}SSL connect error${Color_Off}"
+		echo -e "SSL connect error"
 		return 0
 	fi
 		
 	response_code=$(grep -E "^< HTTP/" <<< $http_response)
-	response_code=$(cut -d " " -f 3- <<< $response_code)
-	echo -e "\t${response_code}"
+	response_code=$(cut -d " " -f 2- <<< $response_code)
+	echo -e "Response - ${response_code}"
 		
 	for header in "${disclosure_headers[@]}"; do
 		match=$(grep -i "^< ${header}:" <<< $http_response)
@@ -89,50 +97,48 @@ function f_http_parse () {
 	done
 	
 	for header in "${e_disclosure_headers[@]}"; do
-		echo -e "\t${Red}${header}${Color_Off}"
+		echo -e "${Red}Disclosure - ${header}${Color_Off}"
 	done
 	
-	if [[ $(grep '200' <<< $response_code) != "" ]]; then
+	if [[ $(grep -P '3\d\d' <<< $response_code) == "" ]]; then
+		for header in "${m_security_headers[@]}"; do
+			echo -e "${Yellow}Missing - ${header}${Color_Off}"
+		done
+		
 		for header in "${e_security_headers[@]}"; do
-			echo -e -n "\t${Green}$(cut -d " " -f 1-4 <<< $header)${Color_Off}"
+			echo -e -n "${Green}Settup - $(cut -d " " -f 1-4 <<< $header)${Color_Off}"
 			if (( $(tr -cd ' ' <<< $header | wc -c) < 4 )); then
 				echo ""
 			else
 				echo -e "${Green} ...${Color_Off}"
 			fi
 		done
-		
-		for header in "${m_security_headers[@]}"; do
-			echo -e "\t${Yellow}${header} - missing${Color_Off}"
-		done
 	fi
 }
 
-function f_resources_check () {
-	domain=$1
-
-    f_dns_resolve $domain
-	if [[ $resolve != 'Unresolved' ]]; then
-   		echo -e -n '\tHTTP: '
-		f_http_parse $domain
-		echo -e -n '\tHTTPS: '
-   		f_http_parse "https://${domain}"
-		echo ""
-	fi
+f_print_help () {
+	echo -e "Usage: headers-lookup [options...] <url>\n" \
+			 "-h\tdisplay this help and exit\n" \
+			 "-c\tcolorized output"
 }
+while getopts "hc" opt; do
+	case $opt in
+		h) 	f_print_help
+			exit ;;
+		c)
+			Red='\033[0;91m'
+			Yellow='\033[0;93m'
+			Green='\033[0;92m'
+			Color_Off='\033[0m';;
+		?) 	exit;;
+	esac
+done
+shift $((OPTIND-1))
 
-param=$1
+url=$1
 
-if [[ $param != "" ]]; then
-	domain=("${param}")
+if [[ $url != "" ]]; then
+	f_http_parse $url
 else
-	domain=("example.com")
-fi
-
-www_subdomain=$(grep "^www\." <<< $domain)
-
-f_resources_check $domain
-
-if [[ $ip_address == "" ]] && [[ $www_subdomain == "" ]]; then
-	f_resources_check "www.${domain}"
+	f_print_help
 fi
