1bf4bd6064d94e1beb590327d0746cce1ace1b60
Andrew Abletsov <abletsoff@gmail.com>
2023-08-05 17:54:05 +0300

    Adjustments

--- http-enum.sh
+++ http-enum.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 user_agent="Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"
+err_file="/tmp/http-enum-stderr"
 
 # last update: "2023-01-12 21:19:59"
 
@@ -57,9 +58,9 @@ function f_cookies_analyzing () {
 	e_disclosure_cookies=() # existing disclosure cookies 
 	
 	for cookie in "${disclosure_cookies[@]}"; do
-		match=$(grep -i "^< Set-Cookie: ${cookie}" <<< $http_response)
+		match=$(grep -i "^Set-Cookie: ${cookie}" <<< $http_response)
 		if [[ $match != "" ]]; then
-			e_disclosure_cookies+=("$(cut -d " " -f 2-3 <<< $match)")
+			e_disclosure_cookies+=("$(cut -d " " -f 1-2 <<< $match)")
 		fi
 	done
 	
@@ -75,16 +76,16 @@ function f_headers_analyzing () {
 	m_security_headers=()	# missed security headers
 
 	for header in "${disclosure_headers[@]}"; do
-		match=$(grep -i "^< ${header}:" <<< $http_response)
+		match=$(grep -i "^${header}:" <<< $http_response)
 		if [[ $match != "" ]]; then
-			e_disclosure_headers+=("$(cut -d " " -f 2- <<< $match)")
+			e_disclosure_headers+=("$(cut -d " " -f 1- <<< $match)")
 		fi
 	done
 	
 	for header in "${security_headers[@]}"; do
-		match=$(grep -i -m 1 "^< ${header}:" <<< $http_response)
+		match=$(grep -i -m 1 "^${header}:" <<< $http_response)
 		if [[ $match != "" ]]; then
-			e_security_headers+=("$(cut -d " " -f 2- <<< $match)")
+			e_security_headers+=("$(cut -d " " -f 1- <<< $match)")
 		else
 			m_security_headers+=("${header}")	
 		fi
@@ -133,60 +134,71 @@ function f_body_analyzing () {
 }
 
 function f_http_parse () {
-	
-	http_response=$(curl -A "${user_agent}" --connect-timeout 5 -v $1 2>&1)
-	
-	if [[ $(grep "^curl: (28)" <<< $http_response) != "" ]]; then
-		echo -e "Warning: Connection timeout"
-		return 0
-	elif [[ $(grep "^curl: (7)" <<< $http_response) != "" ]]; then
-		echo -e "Warning: Failed to connect"
-		return 0
-	elif [[ $(grep "^curl: (56)" <<< $http_response) != "" ]]; then
-		echo -e "Warning: Connection reset"
-		return 0
-	elif [[ $(grep "^curl: (60)" <<< $http_response) != "" ]]; then
+    first_output=$1
+    url=$2
+
+    if [[ $first_output == 'false' ]]; then
+        echo ''
+    fi
+    echo "URL: $url"
+
+    http_response=$(curl -sS -A "${user_agent}" --connect-timeout 5 -i "$url" \
+        2>$err_file | tr '\0' '\n')
+
+    if  [[ $(grep "curl: (60)" $err_file) != "" ]]; then
         echo "Warning: (SSL certificate problem)"
-	    http_response=$(curl -A "${user_agent}" --connect-timeout 5 -v -k $1 2>&1)
-	elif [[ $(grep "^curl: (3)" <<< $http_response) != "" ]]; then
-		echo -e "Error: URL bad/illegal format"
-		return 0
-	elif [[ $(grep "^curl: (52)" <<< $http_response) != "" ]]; then
-		echo -e "Warning: Empty reply from server"
-		return 0
-	elif [[ $(grep "^curl: (35)" <<< $http_response) != "" ]]; then
-		echo -e "Error: SSL connect error"
-		return 0
-	elif [[ $(grep "^curl: (6)" <<< $http_response) != "" ]]; then
-		echo -e "Error: Could not resolve host"
-		return 0
-	fi
-		
-	response_code=$(grep -E "^< HTTP/" <<< $http_response)
-	response_code=$(cut -d " " -f 2- <<< $response_code)
-	echo -e "Response - ${response_code}"
+        http_response=$(curl -sS -k -A "${user_agent}" --connect-timeout 5 -i "$url" \
+         2>$err_file | tr '\0' '\n')
+    fi
+
+    if  [[ $(grep "curl: (" $err_file) != "" ]]; then
+        cat $err_file
+        return 1
+    fi
+	
+	response_code=$(grep -E "^HTTP/" <<< $http_response)
+	echo "Response - ${response_code}"
 	
 	f_cookies_analyzing "$http_response"
 	f_headers_analyzing "$http_response"
 	f_body_analyzing "$http_response"
 	
 	if [[ $(grep -P '30[12378]' <<< $response_code) != "" ]]; then
-		location=$(grep --ignore-case "< location" <<< $http_response)
-		echo $location | cut -d " " -f 2-
+		location=$(grep --ignore-case "location" <<< $http_response)
+		echo "$location"
 		if [[ $follow_redirect == "True" ]]; then
 			url=$(grep --only-matching -P "https?://\S*" <<< $location)
-			f_http_parse "$url"
+			f_http_parse "false" "$url"
 		fi
 	fi
 }
 
+
+f_ip_request () {
+    scheme_host=$(echo "$url" | grep -o -P "(https?:\/\/)?[A-Z,a-z,1-9,\.]+" \
+        | head -n 1)
+    
+    if [[ $(echo "$scheme_host" | grep ":") != '' ]]; then
+        host=$(echo $scheme_host | cut -d ":" -f2 | sed "s/\///g")
+    else
+       host=$scheme_host
+    fi
+
+    resolve=($(dig +short $host | grep -P "$ip_address_regex"))
+    for ip in ${resolve[@]}; do
+        url=$(echo "$url" | sed "s/$host/$ip/g")
+        f_http_parse "false" "$url"
+    done
+}
+
 f_print_help () {
 	echo -e "Usage: headers-lookup [options...] <url>\n" \
-			 "-h\tdisplay this help and exit\n" \
-			 "-c\tcolorized output\n" \
-			 "-f\tfollow redirect"
+			"-h\tdisplay this help and exit\n" \
+			"-c\tcolorized output\n" \
+			"-f\tfollow redirect\n"
+            "-i\tIP address in hostname(e.g. Host: 8.8.8.8)"
 }
-while getopts "hcf" opt; do
+while getopts "hcfi" opt; do
 	case $opt in
 		h) 	f_print_help
 			exit ;;
@@ -196,6 +208,7 @@ while getopts "hcf" opt; do
 			Green='\033[0;92m'
 			Color_Off='\033[0m';;
 		f)	follow_redirect="True";;
+		i)	ip_request="True";;
 		?) 	exit;;
 	esac
 done
@@ -204,7 +217,10 @@ shift $((OPTIND-1))
 url=$1
 
 if [[ $url != "" ]]; then
-	f_http_parse "$url"
+	f_http_parse "true" "$url"
+    if [[ $ip_request = "True" ]]; then
+        f_ip_request
+    fi
 else
 	f_print_help
 fi
